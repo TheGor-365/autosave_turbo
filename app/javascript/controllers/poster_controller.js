f7359b71723ed0a4c8fb5a30b27d0aee3d5c6275
Gor <7495500@gmail.com>
2023-03-20 20:26:29 +0300

    Add stimulus controller logic

--- app/javascript/controllers/poster_controller.js
+++ app/javascript/controllers/poster_controller.js
@@ -1,7 +1,40 @@
 import { Controller } from "@hotwired/stimulus"
+import debounce from "lodash.debounce"
 
 // Connects to data-controller="poster"
 export default class extends Controller {
   connect() {
+    this.form = this.element.closest("form")
+    this.formData = new FormData(this.form)
+    this.url = this.form.action + "/autosave"
+    const autosave_delay = this.form.dataset.autosaveDelay
+    this.autosaveDelayAsInt = parseInt(autosave_delay)
+
+    const requestToDelay = () => this.sendRequest(this.url, this.formData)
+
+    this.debouncedRequest = debounce(requestToDelay, this.autosaveDelayAsInt)
+  }
+
+  save() {
+    this.formData = new FormData(this.form)
+    this.debouncedRequest(this.url, this.formData)
+  }
+
+  sendRequest(url, formData) {
+    console.log("Sending request to " + url)
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+      headers: {
+        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
+        "Accept": "text/vnd.turbo-stream.html"
+      },
+      credentials: "same-origin"
+    }).then((response) => {
+      response.text().then((html) => {
+        document.body.insertAdjacentHTML("beforeend", html)
+      })
+    })
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "posts#index"
-  patch '/posts/:id/autosave', to: 'posts#autosave', to: 'autosave_post'
+  patch '/posts/:id/autosave', to: 'posts#autosave', as: 'autosave_post'
   resources :posts
 end
